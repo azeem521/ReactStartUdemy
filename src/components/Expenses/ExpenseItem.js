474cc82215ce6023685b7df45204f72b0671a053
Mohd <azeemsre1@gmail.com>
2022-12-19 21:11:31 +0530

    deleteExpense via useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
     let date=props.date;
     let title=props.title;
     const amount=props.amount
+
+    const [price,setPrice]=useState(amount);
+
+    const DeleteButton =()=>{
+      setPrice('');
+    }
+
   return (
     <Card className='expense-item'>
-        <div>{date.toString()}</div>
+        <ExpenseDate date={date}/>
     <div className='expense-item__description'>
         <h2>{title}</h2>
     </div>
-    <div className='expense-item__price'>${amount}</div>
-    
+    <div className='expense-item__price'>${price}</div>
+    <button onClick={DeleteButton}>Delete Expense</button>
     </Card>
   )
 }

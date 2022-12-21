b28164fb165315e29dd95a97864db265c6eaacef
Mohd <azeemsre1@gmail.com>
2022-12-21 12:58:33 +0530

    ChallengeConditionalRendering

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -53,6 +53,7 @@ const [amount,setamount]=useState('')
             </div>
         </div>
         <div className="new-expense__actions">
+            <button type='button' onClick={props.onCancel}>Cancel</button>
             <button type="submit">Add Expense</button>
         </div>
         </form>
